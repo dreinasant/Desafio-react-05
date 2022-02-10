b22a56aab1fe805f79e6633d037918cc67c22574
dreinasant <ao.andreina@gmail.com>
2022-02-10 14:41:24 -0300

    Desafio-react-05

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
+import Mulher from './assets/mullher.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+ state = {
+   shortsFeminino: [
+     {
+      marca: "Puma",
+      cor: "Vermelho",
+      tamanho: ["M", "G"],
+      composição: "85% poliéster, 15% elastano"
+     },
+     {
+      marca: "Adidas",
+      cor: "Preto",
+      tamanho: ["P", "M", "G"],
+      composição: "100% poliéster" 
+     },
+     {
+      marca: "Lacoste",
+      cor: "Verde Militar",
+      tamanho: ["P", "G"],
+      composição: "Algodão"
+     },
+     {
+       marca: "Nike",
+       cor: "Rosa",
+       tamanho: ["M", "G"],
+       composição: "100% poliéster"
+     },
+     {
+       marca: "Marisa",
+       cor: "Azul",
+       tamanho: ["P", "M", "G", "GG"],
+       composição: "Jeans"
+     },
+     {
+       marca: "Fila",
+       cor: "Marrom",
+       tamanho: "P",
+       composição: "Poliéster"
+     }
+   ]
+ }
+
+  render(){
+    return(
+      <div>
+        <h1>Shorts Feminino</h1>
+        <img src={Mulher} alt="mulher"/>
+        {this.state.shortsFeminino.map((item) => (
+          <div className="intro">
+              <p>Marca: {item.marca}</p>
+              <p>Cor: {item.cor}</p>
+              <p>Tamanho: {item.tamanho}</p>
+              <p>Composição: {item.composição}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
